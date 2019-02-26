42d1fecc30fb16a61f50577f810b129a9e162fce
Howard Choong <howard.choong@firstrade.com>
2019-02-26 12:46:26 -0500

    added test examples of react components

--- summing-grid/src/tests/App.test.js
+++ summing-grid/src/tests/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from '../App';
+import InputCell from '../InputCell';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('<App />', () => {
+  it('renders three <InputCell /> components', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(InputCell)).toHaveLength(3);
+  });
+
+  it('renders one .cell-total div', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.cell.cell-total')).toHaveLength(1);
+  });
 });
