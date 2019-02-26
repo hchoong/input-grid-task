5c524db41a0ddf829adc65e74a0a18981e6cb53f
Howard Choong <howard.choong@firstrade.com>
2019-02-26 12:16:08 -0500

    implemented styling and functions

--- summing-grid/src/App.js
+++ summing-grid/src/App.js
@@ -1,13 +1,78 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types'
 
-class App extends Component {
+const summing = (arr) => arr.reduce((sum, ele) => sum + Number.parseFloat(ele || 0), 0)
+
+class InputCell extends React.Component {
+  constructor(props) {
+    super(props)
+    this.onInputChange = this.onInputChange.bind(this)
+  }
+  onInputChange(e) {
+    const value = e.target.value
+    this.props.cellUpdated(this.props.index, value)
+  }
+
   render() {
+    const { value } = this.props
+    return (
+      <div className="cell">
+        <input type="text" value={value} onChange={this.onInputChange} placeholder="number" />
+      </div>
+    )
+  }
+}
+
+InputCell.propTypes = {
+  index: PropTypes.number.isRequired,
+  cellUpdated: PropTypes.func.isRequired,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]).isRequired,
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    // func initiated from child to update the parent's state
+    this.cellUpdated = this.cellUpdated.bind(this)
+
+    // parent holds the cell value on the state
+    this.state = {
+      cellValues: Array(3).fill('')
+    }
+  }
+
+  cellUpdated(index, value) {
+    const { cellValues } = this.state
+
+    // don't mutate the original array values directly
+    const newArr = [
+      ...cellValues.slice(0, index),
+      value,
+      ...cellValues.slice(index + 1)
+    ]
+    this.setState({
+      cellValues: newArr
+    })
+  }
+
+  render() {
+    const { cellValues } = this.state
+    const sum = summing(cellValues)
+
     return (
       <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="grid">
+          {cellValues.map((val, index) =>
+            <InputCell key={index} index={index} value={val} cellUpdated={this.cellUpdated} />
+          )}
+          <div className="cell cell-total">
+            {sum}
+          </div>
+        </div>
       </div>
     );
   }
